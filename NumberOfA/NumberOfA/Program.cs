f2b280419432d369cf0f37c41cf47846ca1990d5
user <user@ECHTP-50-18>
2019-03-12 21:49:20 +0300

    NumberOfA

---
+++ NumberOfA/NumberOfA/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userWord;
+            int numberOfA=0;
+            Console.WriteLine("Please enter a word:");
+            userWord = Console.ReadLine();
+            
+            for(int i = 0; i < userWord.Length; i++)
+            {
+
+                if (userWord[i] == 'a')
+                {
+                    numberOfA++;
+                }
+                
+            }
+            Console.WriteLine(numberOfA);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SwitchSample/SwitchSample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Color color = (Color) new Random().Next(0,3);
+            Console.WriteLine(color);
+            Console.Read();
+            switch (color)
+            {
+                case Color.Red:
+                    Console.Write("The color is red");
+                    break;
+                case Color.Green:
+                    Console.Write("The color is green");
+                    break;
+                case Color.Blue:
+                    Console.Write("The color is blue");
+                    break;
+                default:
+                    Console.WriteLine("No color defined");
+                    break;
+            }
+        }
+    }
+
+    public enum Color {Red, Green, Blue};
+}
