7eb74b67aa6c04cf5b606146e2aae0cfee91dc26
user <user@ECHTP-50-18>
2019-03-12 20:09:32 +0300

    Classwork

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkInClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var timeSpan = DateTime.Now.TimeOfDay;
+            TimeSpan time = timeSpan;
+            int hours = time.Hours;
+            Console.WriteLine(hours);
+
+            if (hours <= 12)
+            {
+                Console.WriteLine("Good morning, guys");
+            }
+            else if (hours >= 15)
+            {
+                Console.WriteLine("Good evening, guys");
+            }
+            else
+            {
+                Console.WriteLine("Good day, guys");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
