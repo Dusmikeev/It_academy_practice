98c5da62969c451bee2f0589c10762d865f0eafd
user <user@ECHTP-50-18>
2019-03-14 21:53:24 +0300

    array examples

---
+++ ExampleApp1403/ExampleApp1403/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApp1403
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize;
+            arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[arraySize];
+
+            for(int x = 0; x<arraySize;x++)
+            {
+                array[x] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach (int x in array)
+            {
+                Console.Write(" "+x);
+            }
+            Console.Read();
+        }
+    }
+}
