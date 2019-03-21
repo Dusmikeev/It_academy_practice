87586406b8945599d7cde6c727090df9ac4aa18f
user <user@ECHTP-50-18>
2019-03-21 19:56:14 +0300

    Решать задачи под давлением не моё =)

---
+++ lecture_6_tasks/lecture_6_tasks/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture_6_tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = { };
+            CreateArray();
+            string element1 = "32";
+            string element2 = "A";
+            string element3 = "Hello";
+
+            string[] array2 = { element1, element2, element3 };
+
+            foreach (object x in array2)
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+
+        public void CreateArray()
+        {
+            int[] array = { };
+        }
+
+        
+
+    }
+    
+}
