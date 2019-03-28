befbf38652cae036b02b67b3832a4d616b751cfb
user <user@ECHTP-50-18>
2019-03-28 20:56:57 +0300

    Lecture_7

---
+++ Lecture_7/StringBuilder/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilder_classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //StringBuilder sb = new StringBuilder("Hello, guys!");
+            //Console.WriteLine(sb.Capacity);
+            //Console.WriteLine(sb.Length);
+            //Console.WriteLine(sb.MaxCapacity);
+
+            //sb.Append("\nHello");
+            //Console.WriteLine($"{sb.Capacity}----{sb.Length}");
+
+            string a = "Hello guys";
+            string b = "HELLO GUYS";
+
+            var res = a.Equals(b);
+
+            var str1 = "Hello";
+            var str2 = "my friend";
+            var result = string.Compare(str1, str2);
+
+            var culture = System.Globalization.CultureInfo.CurrentCulture;
+            Console.WriteLine($"{culture.DisplayName}");
+
+            foreach(var item in culture.DateTimeFormat.DayNames)
+            {
+                Console.WriteLine($"{item} - ");
+            }
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ lecture 6 practice/lecture 6 practice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture_6_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str1 = @"\\server\file\file.txt";
+            Console.WriteLine(str1);
+            Console.Read();
+
+            string nullString = null;
+            Console.WriteLine(nullString.Length); //doesnt highlight potential - Nullable reference exception
+
+
+            
+
+        }
+    }
+}
