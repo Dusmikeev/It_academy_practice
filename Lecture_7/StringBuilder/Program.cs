ec8afa8c54426ad7501ab0037300a09e52c05d4e
user <user@ECHTP-50-18>
2019-04-09 20:22:29 +0300

    Practice structs

--- Lecture_7/StringBuilder/Program.cs
+++ Lecture_7/StringBuilder/Program.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace StringBuilder_classwork
 {
+    class Motorcycle
+    {
+
+    }
     class Program
     {
+        
+
         static void Main(string[] args)
         {
 
@@ -19,22 +25,25 @@ namespace StringBuilder_classwork
             //sb.Append("\nHello");
             //Console.WriteLine($"{sb.Capacity}----{sb.Length}");
 
-            string a = "Hello guys";
-            string b = "HELLO GUYS";
+            //string a = "Hello guys";
+            //string b = "HELLO GUYS";
 
-            var res = a.Equals(b);
+            //var res = a.Equals(b);
 
-            var str1 = "Hello";
-            var str2 = "my friend";
-            var result = string.Compare(str1, str2);
+            //var str1 = "Hello";
+            //var str2 = "my friend";
+            //var result = string.Compare(str1, str2);
 
-            var culture = System.Globalization.CultureInfo.CurrentCulture;
-            Console.WriteLine($"{culture.DisplayName}");
+            //var culture = System.Globalization.CultureInfo.CurrentCulture;
+            //Console.WriteLine($"{culture.DisplayName}");
 
-            foreach(var item in culture.DateTimeFormat.DayNames)
-            {
-                Console.WriteLine($"{item} - ");
-            }
+            //foreach(var item in culture.DateTimeFormat.DayNames)
+            //{
+            //    Console.WriteLine($"{item} - ");
+            //}
+
+
+            Motorcycle motorcycle = new Motorcycle();
 
             Console.Read();
 
---
+++ Practice_0904/Project_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle[] rects = new Rectangle[100];
+            Random rnd = new Random();
+            int dublicats = 0;
+            for (int i = 0; i < rects.Length; i++)
+            {
+                rects[i].Widght = rnd.Next(1, 10);
+                rects[i].Height = rnd.Next(1, 10);
+            }
+
+            for (int i = 0; i < rects.Length; i++)
+            {
+                if(rects[0].Widght == rects[i].Widght && rects[0].Height == rects[i].Height)
+                {
+                    dublicats++;
+                }
+                Console.WriteLine(rects[i]);
+            }
+            Console.WriteLine(dublicats);
+            Console.ReadKey();
+        }
+    }
+    struct Rectangle
+    {
+        public int Widght { get; set; }
+        public int Height { get; set; }
+    }
+}
