03c2ac6842d32229ffb54a274bcd2d6dea34a622
user <user@ECHTP-50-18>
2019-04-23 21:13:24 +0300

    String method extensions pratice

---
+++ StringExtension/StringExtension/Extensions/StringExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtension.Extensions
+{
+    public static class StringExtension
+    {
+        public static string StringCut(this string currentString, int replaceIndex)
+        {
+            currentString = currentString.Remove(replaceIndex);
+            currentString = currentString.Insert(replaceIndex, "...");
+            return currentString;
+        }
+
+    }
+}
---
+++ StringExtension/StringExtension/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringExtension.Extensions;
+
+namespace StringExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "I am in a good mood today";
+            Console.WriteLine(test.StringCut(5));
+            Console.Read();
+        }
+    }
+}
